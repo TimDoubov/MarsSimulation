7c8449ba775bea1221a3660ecce93a76440fa976
TimDoubov <timothydoubov101@gmail.com>
2026-06-27 22:30:23 -0500

    Implemented randomized shipments & 100 day limit

--- Mars Simulation/markone.py
+++ Mars Simulation/markone.py
@@ -1,23 +1,34 @@
+#Imports
+import random
 #Basic Variables
 people = 300
 food = 3000
-daily_shipment = 200
-Day = 0
-Survive = True
+max_shipment = 300
+day = 0
+survive = True
+shipment_success = 0
+
+def daily_shipment(m,s):
+    return int(m*s)
+
+
 #Deletes how much food people eat daily
-def daily_food(f, p):
-    return f - p * 3 #+ daily_shipment
-#Loops through day until death
-while Survive == True:
-    Day += 1
-    food = daily_food(food, people)
+def daily_food(f, p, s):
+    return f - p * 3 + s
+#Loops through day until death or day 100
+while survive == True:
+    day += 1
+    shipment_success = random.random()
+    food = daily_food(food, people, daily_shipment(max_shipment, shipment_success))
     if food <= 0:
         people -= int(abs(food/3))
         food = 0
     if people <= 0:
-        Survive = False
+        survive = False
         people = 0
-    print(f"Mars Survival: Day {Day}, People alive: {people}, Food Left: {food}")
+    if day == 100:
+        survive = False
+    print(f"Mars Survival: Day {day}, People alive: {people}, Food Left: {food}, Shipment Success: %{int(shipment_success*100)}")
 
 
 
